360600dceb49d8a1f7c8dea1c3fe479862fa45d6
Saetbyeol Lim <slim62@myseneca.ca>
2022-09-17 21:22:49 -0400

    WEEK02

---
+++ WEEK2/SaetbyeolLim_Lab1b.sql
@@ -0,0 +1,66 @@
+-- ***************************************
+-- DBS311 - Fall 2022
+-- Lab 1b
+-- Review of JOIN statements
+-- 
+-- Saetbyeol Lim
+-- 149814212
+-- 18th Sep 2022
+-- ***************************************
+-- no regular expression
+/* 
+NOTES
+-- Make sure you follow the course style guide for SQL as posted on blackboard.
+-- Data should always be sorted in a logical way, for the question, even if the 
+   question does not specify to sort it.
+*/
+
+-- Q1
+/* 
+Provide a list of ALL departments, what city they are located in, and the name
+of the current manager, if there is one.  
+*/
+SELECT 
+FROM departments
+
+
+-- Q2
+/*
+Allow the user to enter the name of a country, or any part of the name, and 
+then list all employees, with their job title, currently working in that country.
+*/
+SELECT
+FROM
+
+-- Q3
+/*
+Provide a contact list of all employees, and if they have a manager, 
+the name of their direct manager.
+*/
+SELECT
+FROM
+
+
+-- Q4
+/*
+Provide a list of locations in the database, that currently do not have 
+any employees working there.
+*/
+SELECT
+FROM
+
+-- Q5
+/*
+Provide a list of employees whom are currently still in the same job that they
+started in (i.e. they have never changed job titles).
+*/
+SELECT
+FROM
+
+
+
+
+
+
+
+
